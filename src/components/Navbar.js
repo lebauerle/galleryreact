0549a9dc966908f612f66b77dd7e574473e3874c
Your Name <you@example.com>
2021-02-22 21:34:58 +0100

    only stateless components, no functionallity

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React  from "react";
+
+const Navbar = () => {
+    return(
+        <nav className="main-Nav">
+            <ul>
+            <li>Cats</li>
+            <li>Dogs</li>
+            <li>Computers</li>
+            </ul>
+        </nav>
+    )
+};
+export default Navbar;
---
+++ src/components/Picture.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Picture = (props) =>{
+    return (
+        <li>
+            <img src={props.url} alt={props.title} />
+        </li>
+    )
+}
+export default Picture;
